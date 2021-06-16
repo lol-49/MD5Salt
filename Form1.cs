538906859ea048334d2853d75ff20006cb402756
lol-49 <69907830+lol-49@users.noreply.github.com>
2021-06-16 17:04:19 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace MD5_Salt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = MD5(MD5(textBox1.Text + textBox2.Text));
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                label4.Text = "Type Something";
+                label4.ForeColor = Color.Red;
+                textBox3.Text = (String.Empty);
+            }
+            else
+            {
+                label4.Text = "Succesfully Hash";
+                label4.ForeColor = Color.Green;
+            }
+        }
+
+        string MD5(string input)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = md5.ComputeHash(Encoding.Unicode.GetBytes(input));
+            string output = BitConverter.ToString(bytes);
+            return output.ToLower().Replace("-", String.Empty);
+        }
+
+        [STAThread]
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox3.Text);
+        }
+    }
+}
